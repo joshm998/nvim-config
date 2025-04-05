5bc255de5afa5f9cb66e405f06a2f92de973ba9d
Josh Mangiola <contact@joshmangiola.com>
2025-04-05 23:31:08 +1030

    Fixes Deprecated Neovide Opacity Value

--- lua/nvim_basic_config/neovide.lua
+++ lua/nvim_basic_config/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-  vim.g.neovide_transparency = 0.9
+  vim.g.neovide_opacity = 0.9
   vim.g.neovide_window_blurred = true
   vim.g.neovide_no_idle = false
   vim.g.neovide_fullscreen = false
